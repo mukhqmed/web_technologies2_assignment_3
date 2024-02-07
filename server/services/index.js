aff780ffe9c7dcd44a8b8fde273523342b21f1db
mukhqmed <123320324+mukhqmed@users.noreply.github.com>
2024-02-08 04:14:24 +0600

    Delete server/services/index.js

--- server/services/index.js
+++
@@ -1,9 +0,0 @@
-exports.generateCrudMethods = Model => {
-    return {
-        getAll: () => Model.find(),
-        getById: id => Model.findById(id),
-        create: record => Model.create(record),
-        update: (id, record) => Model.findByIdAndUpdate(id, record, { new: true}),
-        delete: id => Model.findByIdAndDelete(id)
-    }
-}
