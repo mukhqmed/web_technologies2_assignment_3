f6a00e7b5fb7f27b85a3061af7f30d769bf1771e
mukhqmed <123320324+mukhqmed@users.noreply.github.com>
2024-02-08 04:11:24 +0600

    Update blog.model.js

--- server/models/blog.model.js
+++ server/models/blog.model.js
@@ -1,8 +1,10 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-module.exports = mongoose.model('Blog', {
+const blogSchema = mongoose.Schema({
     title: { type: String, required: true },
     body: { type: String, required: true },
     author: { type: String, required: true },
     timestamps: { type: Date, default: Date.now }
 });
+
+module.exports = mongoose.model('Blog', blogSchema);
